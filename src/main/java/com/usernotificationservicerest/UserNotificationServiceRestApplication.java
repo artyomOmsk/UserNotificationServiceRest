a13f77f2451c629f20561c10be80c8dbcb2987ea
Lyamin Artyom <lyaminartyom07@gmail.com>
2023-08-27 11:06:24 +0300

    Первый коммит

---
+++ src/main/java/com/usernotificationservicerest/UserNotificationServiceRestApplication.java
@@ -0,0 +1,13 @@
+package com.usernotificationservicerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotificationServiceRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserNotificationServiceRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usernotificationservicerest/UserNotificationServiceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usernotificationservicerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNotificationServiceRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
